d7bb61ace1ea40ab50a64bd411bd4345066626cf
Udeh Udochukwu <udochukwugodwin13@gmail.com>
2024-04-07 19:42:17 +0000

    Second Commit

--- frontend/README.md
+++ frontend/README.md
@@ -6,3 +6,6 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+React saves us the use of if statements. for example in the setting of dark mode and light mode, we can easily put the various features into effect by putting them within the ClassName classification.
+
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import{IoMdSearch } from "react-icons/io";
+import { FaCaretDown, FaShoppingCart } from "react-icons/fa";
+import DarkMode from './DarkMode';
+
 const MenuLinks = [
   {
     id:1,
@@ -21,11 +25,30 @@ const MenuLinks = [
     link: "/#blog",
   },
 ]
+
+const DropdownLinks = [
+  {
+    id: 1,
+    name: "Trending products",
+    link: "/#",
+  },
+  {
+    id: 1,
+    name: "Best Selling",
+    link: "/#",
+  },
+  {
+    id: 1,
+    name: "Top Rated",
+    link: "/#",
+  },
+]
+
 const Navbar = () => {
   return (
     <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
         <div className='py-4'>
-            <div className="container">
+            <div className="container flex justify-between items-center">
                 {/* Logo and Links section */}
                 <div className='flex items-center gap-4'>
                   <a href="#"
@@ -37,28 +60,73 @@ const Navbar = () => {
                   {/* Menu Items */}
                   <div className='hidden lg:block'>
                     <ul className='flex items-center gap-4'>
-                      {
-                        MenuLinks.map((data, index) =>(
+                      {MenuLinks.map((data, index) =>(
                           <li key={index}>
-                            <a href={data.link}
-                            className='inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200'
-                            >{data.name}</a>
+                            <a 
+                            href={data.link}
+                            className='inline-block px-4 font-semibold
+                             text-gray-500 hover:text-black
+                             dark:hover:text-white duration-200'
+                            >
+                              {""}
+                              {data.name}
+                              </a>
                           </li>    
                         ))}
+                      {/* Dropdown */}
+                        <li className='relative cursor-pointer group'>
+          <a href="#"
+           className='flex items-center gap-[2px] font-semibold text-gray-500 dark:hover:text-white py-2  '>
+          Quick Links
+          <span>
+              <FaCaretDown className='group-hover:rotate-180 duration-300'></FaCaretDown>
+          </span>
+          </a>
+          {/* dropdown links */}
+          <div className='absolute z-[9999] hidden group-hover:block
+           w-[200px] rounded-md bg-white shadow-md dark:bg-gray-900 p-2 dark:text-white '>
+                <ul className='space-y-2'>
+                  {
+                    DropdownLinks.map((data, index) => (
+                        <li>
+                          <a 
+                          className='text-gray-500 dark:hover:text-white
+                          duration-200 inline-block w-full  p-2 hover:bg-primary/20
+                          rounded-md font-semibold '
+                          href={data.link}>
+                            {data.name}
+                          </a>
+                        </li>
+                    ))
+                  }
+                </ul>
+
+          </div>
+                        </li>
                     </ul>
                   </div>
                 </div>
                 {/* Navbar Right Section */}
-                <div >
+                <div className='flex justify-between items-center gap-4'>
                   {/* search bar section */}
-                  <div>
-                    <input type="text"
-                    placeholder='search'
-                    className='' />
-                  <IoMdSearch/>
+                  <div className='relative group hidden sm:block'>
+                    <input type="text" placeholder='search' className='search-bar' />
+                  <IoMdSearch
+                  className='text-xl text-gray-600 group-hover:text-primary dark:text-gray-400 absolute top-1/2
+                  -translate-y-1/2 right-3 duration-200 '  />
                   </div>
+                    {/* order btn section       */}
+                    <button className='relative p-3 '>
+                     <FaShoppingCart className='text-xl text-gray-600 dark:text-gray-400'/>
+                     <div className='w-4 h-4  bg-red-500 text-white rounded-full absolute top-0 right-0 flex items-center
+                     justify-center text-xs '>
+                          4
+                     </div>
+                      </button>      
                    {/* Dark Mode section */}
-                  <div></div>
+                  <div>
+                    <DarkMode  />
+                  </div>
                 </div>
             </div>
         </div>
